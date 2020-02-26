191742771f9baec35d7a0da88c44f5b8f88e2068
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2020-02-26 11:54:00 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Basic Svelte app with Parcel
+
+https://dev.to/alexparra/basic-svelte-app-with-parcel-30i5
---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let name;
+</script>
+
+<style>
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  }
+
+  h1 {
+    text-align: center;
+    color: brown;
+  }
+</style>
+
+<div class="container">
+  <h1>Hello {name}!</h1>
+</div>
